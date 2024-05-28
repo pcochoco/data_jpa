b4c053cec49d063ab4ebd02ca47100040a7eea42
pcochoco <cochocolatecoo@gmail.com>
2024-05-29 01:46:47 +0900

    auditing with data jpa

--- src/main/java/jpa/data_jpa/DataJpaApplication.java
+++ src/main/java/jpa/data_jpa/DataJpaApplication.java
@@ -2,9 +2,21 @@ package jpa.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing //스프링 부트 설정 클래스에 적용
 @SpringBootApplication
 public class DataJpaApplication {
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		//AuditorAware : spring bean으로 등록 -> 등록자, 수정자 처리
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
---
+++ src/main/java/jpa/data_jpa/domain/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package jpa.data_jpa.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+/*
+    저장 시점에 등록일, 수정일 등이 저장됨
+    -> 데이터가 중복되는 것 같지만 유지보수에 편리
+    -> 아니면 null값이 들어가 query가 복잡해짐
+ */
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
